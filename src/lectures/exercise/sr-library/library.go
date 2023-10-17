2ce6a8a34442a1747fc93e2fcc0965de0764d5bb
YerkinBS <baizhanoverkin@gmail.com>
2023-10-18 00:36:21 +0600

    added

--- src/lectures/exercise/sr-library/library.go
+++ src/lectures/exercise/sr-library/library.go
@@ -19,8 +19,92 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Status struct {
+	checkedOut string
+	checkedIn  string
+	member     string
+}
+
+type Book struct {
+	name   string
+	status Status
+}
+
+type Library struct {
+	members []string
+	books   []Book
+}
+
+func getPos(lib *Library, bookName string) (bool, int) {
+	bookFound, bookPos := false, -1
+	for i, book := range lib.books {
+		if book.name == bookName {
+			bookFound = true
+			bookPos = i
+		}
+	}
+	return bookFound, bookPos
+}
+
+func timeFormatChanger(timeNow time.Time) string {
+	return timeNow.Format("2006-01-02 15:04:05")
+}
+
+func checkOut(lib *Library, bookName string) {
+	bookFound, bookPos := getPos(lib, bookName)
+
+	if !bookFound {
+		fmt.Println("\nThis book is not registered in the library. Try to choose another book. Thanks!")
+	} else {
+		if lib.books[bookPos].status.checkedOut == "" {
+			lib.books[bookPos].status.checkedOut = timeFormatChanger(time.Now())
+			lib.books[bookPos].status.checkedIn = ""
+			lib.books[bookPos].status.member = lib.members[rand.Intn(len(lib.members))]
+			fmt.Println("\nThe book named", bookName, "is checked out succesfully by", lib.books[bookPos].status.member)
+		} else {
+			fmt.Println("\nThe book named", bookName, "has already been checked out by", lib.books[bookPos].status.member, "in", lib.books[bookPos].status.checkedOut)
+		}
+	}
+}
+
+func checkIn(lib *Library, bookName string) {
+	bookFound, bookPos := getPos(lib, bookName)
+
+	if !bookFound {
+		fmt.Println("\nThis book is not registered in the library. Try to choose another book. Thanks!")
+	} else {
+		if lib.books[bookPos].status.checkedIn == "" {
+			lib.books[bookPos].status.checkedIn = timeFormatChanger(time.Now())
+			lib.books[bookPos].status.checkedOut = ""
+			lib.books[bookPos].status.member = lib.members[rand.Intn(len(lib.members))]
+			fmt.Println("\nThe book named", bookName, "is checked in succesfully by", lib.books[bookPos].status.member)
+		} else {
+			fmt.Println("\nThe book named", bookName, "has already been checked in by", lib.books[bookPos].status.member, "in", lib.books[bookPos].status.checkedIn)
+		}
+	}
+}
 
 func main() {
+	library := Library{
+		members: []string{"Yerkin", "Aidana", "Nurtay", "Ambal"},
+		books: []Book{
+			{name: "Eternal Echoes: A Time Travel Adventure"},
+			{name: "The Enchanted Forest Chronicles"},
+			{name: "The Midnight Library"},
+			{name: "The Lost Symbol"},
+		},
+	}
 
+	checkOut(&library, "The Lost Symbol")
+	fmt.Println("\n", library)
+	checkIn(&library, "The Midnight Library")
+	fmt.Println("\n", library)
+	checkIn(&library, "The Midnight Library")
+	checkOut(&library, "The Lost Symbol")
 }
